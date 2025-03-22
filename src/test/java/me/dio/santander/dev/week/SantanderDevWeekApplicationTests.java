296ff3f8bed66cc58128cc6f9ab15d3543d0006e
Tiago Palomares <tiagopalomares@gmail.com>
2025-03-22 12:51:13 -0300

    Initial commit

---
+++ src/test/java/me/dio/santander/dev/week/SantanderDevWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.santander.dev.week;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

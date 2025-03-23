5824519a5adc27bc3b86f1b3108b18f8f16249e3
Tiago Palomares <tiagopalomares@gmail.com>
2025-03-23 14:12:42 -0300

    Atualização do Projeto Santander Dev Week

--- src/test/java/me/dio/SantanderDevWeek2023ApplicationTests.java
+++ src/test/java/me/dio/SantanderDevWeek2023ApplicationTests.java
@@ -1,10 +1,10 @@
-package me.dio.santander.dev.week;
+package me.dio;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SantanderDevWeekApplicationTests {
+class SantanderDevWeek2023ApplicationTests {
 
 	@Test
 	void contextLoads() {
